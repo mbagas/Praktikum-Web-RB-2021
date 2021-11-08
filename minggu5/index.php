5e86f7f089d2af0aba00b247ee23217ef8a6a1e6
mbagas <karab4946@gmail.com>
2021-11-08 16:49:04 +0700

    praktikum mg 5

---
+++ minggu5/index.php
@@ -0,0 +1,49 @@
+<?php
+function operator(){
+    $a = 10;
+    $b = 5;
+    
+    echo "Bilangan 1 : " . $a . "<br> Bilangan 2 : " . $b;
+    
+    echo "<br><br>berikut merupakan hasil dari setiap operasi ";
+    
+    echo "<br><br>PENJUMLAHAN <br> Operator : + <br> Hasil : ".($a+$b);
+    echo "<br><br>PENGURANGAN <br> Operator : - <br> Hasil : ".($a-$b);
+    echo "<br><br>PERKALIAN <br> Operator : * <br> Hasil : ".($a*$b);
+    echo "<br><br>PEMBAGIAN <br> Operator : / <br> Hasil : ".($a/$b);
+    echo "<br><br>MODULUS <br> Operator : % <br> Hasil : ".($a%$b);
+}
+
+
+function aray_random(){
+    echo "<br><br> RANDOM ARRAY<br><br>";
+
+    $arr = array("larine", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat", "tomas", "jojo", "anton");
+    shuffle($arr);
+    
+    foreach ($arr as $data){
+        echo "$data<br>";
+    }
+}
+
+
+
+function prima(){
+    for($i = 2; $i <=50; $i++){
+        for($j = 2; $j<$i ;$j++){
+
+            if($i % $j == 0){
+                break;
+
+            }
+            else if ($i-1 == $j){
+                echo "$i ";
+                break;
+            }
+        }
+    }
+}
+operator();
+aray_random();
+prima();
+?>
